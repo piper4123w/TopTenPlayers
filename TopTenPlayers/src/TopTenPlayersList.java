ec7e875123236144972f3ea10eaed4494478e8a7
piper4123w <kyle.lawson@marquette.edu>
2014-10-11 16:54:58 -0500

    here

--- TopTenPlayers/src/TopTenPlayersList.java
+++ TopTenPlayers/src/TopTenPlayersList.java
@@ -1,24 +1,84 @@
 public class TopTenPlayersList {
-  private LLNode<Player> list;
+
+  int            size;
+
+  private Player first;
+  private Player last;
 
   public TopTenPlayersList() {
-    list = new LLNode<Player>(null);
+    first = null;
+    last = null;
   }
-  
-  public void addEntry(String name, double score) {
-    list
+
+  public boolean isEmpty() {
+    return first == null;
   }
-  
-  public void removeFirst() {
-    
+
+  public void addEntry(String name, int score) {
+    Player newPlayer = new Player(name, score);
+    Player previous = null;
+    Player current = first;
+
+    while (current != null && score < current.getScore()) {
+      previous = current;
+      current = current.next;
+    }
+
+    if (previous == null) {
+      first = newPlayer;
+    }
+    else {
+      previous.next = newPlayer;
+    }
+    newPlayer.next = current;
+    removeDuplicate(name);
   }
-  
-  public void removeLast() {
-    
+
+  private void removeDuplicate(String name) {
+    Player previous = null;
+    Player current = first;
+
+    while (current != null && !current.getName().equals(name)) {
+      previous = current;
+      current = current.getNext();
+    }
+    previous = current;
+    current = current.getNext();
+
+    while (current != null && current.getName().equals(name)) {
+      previous.setNext(current.getNext());
+    }
   }
-  
+
+  public Player removeFirst() {
+    Player temp = first;
+    first = first.next;
+    return temp;
+  }
+
+  public Player removeLast() {
+    Player temp;
+    if(first == null) {
+      return null;
+    }
+    if(first.getNext() == null) {
+      temp = first;
+      first = null;
+    }
+    else {
+      Player current = first;
+      while(current.getNext().getNext() != null) {
+        current = current.getNext();
+      }
+      temp = current.getNext();
+      current.setNext(null);
+    }
+    size--;
+    return temp;
+  }
+
   public String printList() {
     return "";
   }
-  
+
 }
