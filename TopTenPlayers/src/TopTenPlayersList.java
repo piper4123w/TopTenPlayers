c8891642a89ddb3899c21fc9233ab7c9de0a9c05
piper4123w <kyle.lawson@marquette.edu>
2014-10-15 12:19:36 -0500

    Formatted for submission

--- TopTenPlayers/src/TopTenPlayersList.java
+++ TopTenPlayers/src/TopTenPlayersList.java
@@ -139,3 +139,40 @@ public class TopTenPlayersList {
   }
 
 }
+
+
+class Player {
+  private String name;
+  private double score;
+  private Player nextPlayer;
+  
+  public Player(String name, double score, Player nextPlayer) {
+    this.name = name;
+    this.score = score;
+    this.nextPlayer = nextPlayer;
+  }
+
+  public double getScore() {
+    return score;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setScore(int score) {
+    this.score = score;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public Player getNext() {
+    return nextPlayer;
+  }
+  
+  public void setNext(Player nextPlayer) {
+    this.nextPlayer = nextPlayer;
+  }
+}
